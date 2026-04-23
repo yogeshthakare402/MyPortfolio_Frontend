c1067b15bfc2b8985230256f0e2f9c6ab6bbc4d3
yogesh.thakare <yogesh.thakare@instinctinnovations.com>
2026-04-23 22:58:32 +0530

    deployment changes for netlify

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   turbopack: {
     root: __dirname,
   },
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
 };
